12fedaf5609dd6252ad4e420435b01c9f49d63e3
agaio <agaio@123456>
2025-04-15 08:40:36 +0800

    tabbar引入与layout组件实现

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,7 +1,27 @@
 <template>
-  <div>layout</div>
   <RouterView />
+  <van-tabbar v-model="active">
+    <van-tabbar-item
+      v-for="item in router.options.routes[0].children"
+      :key="item.path"
+      icon="item.meta.icon"
+      :url="`#/${item.path}`"
+      >{{ item.meta.name }}</van-tabbar-item
+    >
+  </van-tabbar>
 </template>
 
 <script setup>
+import { ref, onMounted } from "vue";
+import { useRoute, useRouter } from "vue-router";
+
+const active = ref(0);
+const router = useRouter();
+const route = useRoute();
+onMounted(() => {
+  const data = router.options.routes[0];
+  active.value = data.children.findIndex(
+    (item) => "/" + item.path === route.path
+  );
+});
 </script>
